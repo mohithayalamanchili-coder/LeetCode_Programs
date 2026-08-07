9b25b35dd9b41a159c8b8155ea2afd4177a0b081
mohithayalamanchili-coder <mohithayalamanchili@gmail.com>
2026-08-07 10:36:44 +0530

    Time: 49 ms (45.61%) | Memory: 19.7 MB (69.24%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -4,6 +4,7 @@ class Solution:
         d2={}
         for i in p:
             d2[i]=d2.get(i,0)+1
+        #count the frequencies of characters in substring into d1    
         left=0
         d1={}
         ans=[]
@@ -11,18 +12,16 @@ class Solution:
         for right in range(len(s)):
             #d1={}
             #ans=[]
-           
-          
-                
-            d1[s[right]]=d1.get(s[right],0)+1 
-            if right>=len(p)-1:
-                if d1==d2:
-                    ans.append(left)
+            for j in s[right]:
+                d1[s[right]]=d1.get(s[right],0)+1  
+                if right>=len(p)-1: #checking the validity of window
+                    if d1==d2: # comparing hashmaps to check anagrams
+                        ans.append(left) #if anagram adding start index to ans
                     # removing the outgoing element- left
-                d1[s[left]]-=1
-                if d1[s[left]]==0:
-                    d1.pop(s[left])
-                left+=1
+                    d1[s[left]]-=1
+                    if d1[s[left]]==0:
+                        d1.pop(s[left])
+                    left+=1
         return ans                
                               
         
